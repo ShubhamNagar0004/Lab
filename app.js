0bf6b446483e54e630660de1470ebbf2a3f1749b
ShubhamNagar0004 <subhamnagar245@gmail.com>
2026-01-14 11:05:21 +0530

    `Added new code to app.js: imported math module, called math functions, defined a person object and destructured its name property.`

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ let math = require("./math");
 
 console.log(math.sum(3, 6));
 console.log(math.product(3, 8));
+
+let person = {
+  name: "abc",
+};
+
+let { name } = person;
+console.log(name);
+s;
